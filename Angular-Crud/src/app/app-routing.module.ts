9c305ac44160ded53eb5c453fe33b21e994f67af
mariano <marianomadou@gmail.com>
2019-12-17 16:26:11 -0300

    mejorando repo

--- Angular-Crud/src/app/app-routing.module.ts
+++ Angular-Crud/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
+  ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- Angular-Crud/src/app/app.module.ts
+++ Angular-Crud/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { MatListModule } from '@angular/material/list';
     ListCustomersComponent,
     ToolbarComponent,
     FormComponent,
-    MainNavComponent
+    MainNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
